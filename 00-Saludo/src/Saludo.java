a421e13f35cc6abb2666cba1236c5c808e7daa8b
heriberto <hmelizarraraz@hotmail.com>
2016-01-21 13:18:31 -0600

    Saludo

---
+++ 00-Saludo/src/Saludo.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Heriberto
+ *
+ */
+public class Saludo {
+
+	public static void main(String[] args) {
+		//JOptionPane.showMessageDialog(null, "Hola Heriberto");
+		System.out.println("Hola, desde consola");
+	}
+
+}
