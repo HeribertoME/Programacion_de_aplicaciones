b894182391fec7130d88401824526c844048c0f3
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2016-06-30 13:16:28 -0500

    Add Factory Method

---
+++ 16-FactoryMethod/src/Animal.java
@@ -0,0 +1,8 @@
+
+public interface Animal {
+	public static final int PERRO = 1;
+	public static final int GATO =2 ;
+	public static final int ZORRO = 3;
+	
+	public String comer();
+}
---
+++ 16-FactoryMethod/src/Gato.java
@@ -0,0 +1,9 @@
+
+public class Gato implements Animal {
+
+	@Override
+	public String comer() {
+		// TODO Auto-generated method stub
+		return "EL gato come whiskas";
+	}
+}
