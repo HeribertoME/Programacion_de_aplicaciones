d0be937222995371b6eb1943d7a5b75570e2234a
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2017-02-11 11:57:27 -0600

    Se modifica la clase Conexion

--- 13-Conexion a BD/src/Conexion.java
+++ 13-Conexion a BD/src/Conexion.java
@@ -5,18 +5,19 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 /**
- * 
+ *
  * @author heriberto
  *
  */
 public final class Conexion {
-	
+
 	private static final Conexion con = new Conexion();
 	private String nombreBD;
 	private String user;
 	private String pass;
 	private String url;
-	
+	private Connection c = null;
+
 	/**
 	 * Constructor
 	 */
@@ -26,50 +27,61 @@ public final class Conexion {
 		pass = null;
 		url = "jdbc:mysql://localhost/"+nombreBD;
 	}
-	
+
 	/**
 	 * Metodo que retorna la unica instancia generada en esta clase
-	 * 
+	 *
 	 * @return instancia unica
 	 */
 	public static Conexion instanciaConexion (){
 		return con;
 	}
-	
+
 	/**
 	 * Metodo de conexion
 	 */
 	public Connection conectar(){
 		try{
+			// Driver JDBC
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection c = DriverManager.getConnection(url,user,pass);
-			JOptionPane.showMessageDialog(null,
-					"Conexión exitosa",
-					"Éxito",
-					JOptionPane.INFORMATION_MESSAGE);
-			return c;
+			c = DriverManager.getConnection(url,user,pass);
 		} catch (ClassNotFoundException e){
-			JOptionPane.showMessageDialog(null, 
-					"No se encontró la clase Driver\n "+e,
+			JOptionPane.showMessageDialog(null,
+					"No se encontrÃ³ la clase Driver\n " + e,
 					"Error",
-					JOptionPane.ERROR_MESSAGE);			
+					JOptionPane.ERROR_MESSAGE);
+					c = null;
 		} catch (SQLException e){
-			JOptionPane.showMessageDialog(null, 
-					"No se pudó conectar a la base\n "+e,
+			JOptionPane.showMessageDialog(null,
+					"No se pudÃ³ conectar a la base\n " + e,
 					"Error",
 					JOptionPane.ERROR_MESSAGE);
+					c = null;
 		} catch (InstantiationException e) {
-			JOptionPane.showMessageDialog(null, 
-					"No se pudó instanciar la clase\n "+e,
+			JOptionPane.showMessageDialog(null,
+					"No se pudÃ³ instanciar la clase\n " + e,
 					"Error",
 					JOptionPane.ERROR_MESSAGE);
+					c = null;
 		} catch (IllegalAccessException e){
-			JOptionPane.showMessageDialog(null, 
-					"Acceso no permitido a la base de datos\n "+e,
+			JOptionPane.showMessageDialog(null,
+					"Acceso no permitido a la base de datos\n " + e,
 					"Error",
 					JOptionPane.ERROR_MESSAGE);
+					c = null;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null,
+					"Error en la conexion\n " + e,
+					"Error",
+					JOptionPane.ERROR_MESSAGE);
+					c = null;
+		} finally {
+			JOptionPane.showMessageDialog(null,
+					"ConexiÃ³n exitosa",
+					"Ã‰xito",
+					JOptionPane.INFORMATION_MESSAGE);
+					return c;
 		}
-		return null;
 	}
-	
+
 }
